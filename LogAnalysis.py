cd3acb8d9a6cee5371c579dbd99f50525eb655c9
Ajithkumar <eajithkumar128@gmail.com>
2018-12-08 03:57:49 +0530

    Logic to list the most popular articles

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -11,11 +11,30 @@ def createDB():
     return db.connect(database="news")
 
 
+def printData(data):
+    for results in data:
+        print("* {} -- {} VIEWS".format(results[0],results[1]));
+
+
 def mostPopularArticle():
     conn = createDB()
-    print(conn);
-    conn.close();
+    curs = conn.cursor();
+    FetchQuery = """
+        select ar.title,count(lo.path) from articles ar, log lo
+               where substring(lo.path,10) = ar.slug
+               group by lo.path,ar.title
+               order by count(lo.path) desc limit 5
+    """
+    curs.execute(FetchQuery);
+    popularArticles = curs.fetchall();
+    printData(popularArticles)
+    conn.close()
 
 if __name__ == '__main__':
-
+    print("******************************************************************")
+    print("*************************LOGS ANALYSIS****************************")
+    print("******************************************************************")
+    print();
+    print("MOST POPULAR ARTICLES:")
     mostPopularArticle()
+    print("\nMOST POPULAR ARTIST");
