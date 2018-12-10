6a222bc0c2a1c2984cac8d970d39b91e5a5a3ffe
Ajithkumar <eajithkumar128@gmail.com>
2018-12-10 23:42:14 +0530

    Formatting the code to PEP8 standard

--- LogAnalysis.py
+++ LogAnalysis.py
@@ -1,6 +1,7 @@
 '''
     Author: Ajithkumar
-    Description: To analyse the logs from the web server and database and create stats
+    Description: To analyse the logs from the web server and database
+    and create stats
     This program include pyscopg2 module which is used to connect database
 '''
 
@@ -13,27 +14,28 @@ def createDB():
 
 def printData(data):
     for results in data:
-        print("\t\t* {} -- {} VIEWS".format(results[0],results[1]));
+        print("\t\t* {} -- {} VIEWS".format(results[0], results[1]))
     print()
 
 
 def mostPopularArticle():
     conn = createDB()
-    curs = conn.cursor();
+    curs = conn.cursor()
     FetchQuery = """
         select ar.title,count(lo.path) as views from articles ar, log lo
                where substring(lo.path,10) = ar.slug
                group by ar.title
                order by views desc limit 5
     """
-    curs.execute(FetchQuery);
-    popularArticles = curs.fetchall();
+    curs.execute(FetchQuery)
+    popularArticles = curs.fetchall()
     printData(popularArticles)
     conn.close()
 
+
 def mostPopularAuthor():
     conn = createDB()
-    curs = conn.cursor();
+    curs = conn.cursor()
     FetchQuery = """
         select au.name, count(lo.path) as views from log lo
                join articles ar on substring(lo.path,10) = ar.slug
@@ -41,14 +43,15 @@ def mostPopularAuthor():
                group by name
                order by views desc
     """
-    curs.execute(FetchQuery);
+    curs.execute(FetchQuery)
     popularAuthors = curs.fetchall()
-    printData(popularAuthors);
+    printData(popularAuthors)
     conn.close()
 
+
 def mostErroredDays():
-    conn = createDB();
-    curs = conn.cursor();
+    conn = createDB()
+    curs = conn.cursor()
     FetchQuery = """
         select trim(to_char(a.loo::date,'Month'))||
                     to_char(a.loo::date,' DD,YYYY'),
@@ -64,13 +67,13 @@ def mostErroredDays():
 
 
 if __name__ == '__main__':
-    print("\t******************************************************************")
-    print("\t*************************LOGS ANALYSIS****************************")
-    print("\t******************************************************************")
-    print();
+    print("\t***************************************************************")
+    print("\t*************************LOGS ANALYSIS*************************")
+    print("\t***************************************************************")
+    print()
     print("\tMOST POPULAR ARTICLES:")
     mostPopularArticle()
-    print("\tMOST POPULAR ARTIST:");
+    print("\tMOST POPULAR ARTIST:")
     mostPopularAuthor()
-    print("\tREQUEST ERRORS MORE THAN A PERCENTAGE:");
+    print("\tREQUEST ERRORS MORE THAN A PERCENTAGE:")
     mostErroredDays()
